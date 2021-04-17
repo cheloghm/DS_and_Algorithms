4b0e012dc80bfc4ee9365308a97c9319012abf8f
graham chukwumaobi <cheloghm@gmail.com>
2021-04-17 06:01:13 +0100

    Add project files.

---
+++ DS_and_Algorithms/Find_nemo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DS_and_Algorithms
+{
+    public class Find_nemo
+    {
+        StringBuilder sb = new StringBuilder();
+        public void findNemo(string sb1)
+        {
+            for (int i = 0; i < sb1.Length; i++)
+            {
+                sb.Insert(i, sb1[i]);                
+            }
+
+            if (sb.ToString() == "nemo")
+            {
+                Console.WriteLine("Found NEMO");
+            }
+            else
+            {
+                Console.WriteLine("NEMO Not Found");
+            }
+
+        }
+
+        public void findNemo2(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    sb.Insert(j, arr[i][j]);
+                }
+                if (sb.ToString() != "nemo")
+                {
+                    sb.Clear();
+                }
+                else if (sb.ToString() == "nemo")
+                {
+                    Console.WriteLine("Found NEMO");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("NEMO Not Found");
+                }
+            }
+        }
+    }
+}
---
+++ DS_and_Algorithms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DS_and_Algorithms
+{
+    class Program
+    {
+        const string Nemo = "nemo";
+        public static string[] everyone = new string[] { "dory", "bruce", "marlin", "gill", "bloat", "nigel", "squirt", "darla", "hank" };
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Find_nemo nemo = new Find_nemo();
+
+            var watch = new System.Diagnostics.Stopwatch();
+            //nemo.findNemo(Nemo);
+            nemo.findNemo2(everyone);
+            watch.Stop();
+
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+    }
+}
